2f4d1ee5a182de31cc5c58abb7878f697429e817
Nivesh <57831854+zenxey@users.noreply.github.com>
2023-12-17 21:57:08 +0530

    zenxey: updated to pool for better performance

--- Backend/app.js
+++ Backend/app.js
@@ -7,26 +7,27 @@ require('dotenv').config();
 
 const app = express();
 
-// Create MySQL connection
-const dbPassword = process.env.DB_PASSWORD ? decodeURIComponent(process.env.DB_PASSWORD) : '';
-const dbConfig = {
+// Create MySQL connection pool
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: dbPassword,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: 3306,
-};
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
-const connection = mysql.createConnection(dbConfig);
-
-connection.connect((err) => {
+// Check MySQL connection
+pool.getConnection((err, connection) => {
   if (err) {
     console.error('Error connecting to MySQL:', err);
   } else {
     console.log('Connected to MySQL successfully');
+    connection.release(); // Release the connection back to the pool when done
   }
 });
-
 
 // Use cors middleware with explicit origin
 app.use(cors({
@@ -38,3 +39,9 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(require('./router/route'));
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
